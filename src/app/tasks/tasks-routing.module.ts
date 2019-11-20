a192ddcbc96226dbfd569c598ed1e44d5cae7fb4
Alberto Basalo <albertobasalo@hotmail.com>
2019-11-20 19:01:47 +0100

    view task

--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
 import { TasksComponent } from './tasks.component';
 
-const routes: Routes = [{ path: '', component: TasksComponent }];
+const routes: Routes = [
+  { path: '', component: TasksComponent },
+  {
+    path: ':id',
+    loadChildren: () => import('./task/task.module').then(m => m.TaskModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class TasksRoutingModule { }
+export class TasksRoutingModule {}
