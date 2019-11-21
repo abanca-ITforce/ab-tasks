aeeaf2b3e094eb9010f0c9f14b8073bba264dad4
Alberto Basalo <albertobasalo@hotmail.com>
2019-11-21 16:15:11 +0100

    ng g @angular/material:nav layout/nav-shell

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,15 +5,23 @@ import { CopyrightComponent } from './shell/copyright/copyright.component';
 import { MainContentComponent } from './shell/main-content/main-content.component';
 import { ShellComponent } from './shell/shell.component';
 import { TopBarComponent } from './shell/top-bar/top-bar.component';
+import { NavShellComponent } from './nav-shell/nav-shell.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     ShellComponent,
     TopBarComponent,
     CopyrightComponent,
-    MainContentComponent
+    MainContentComponent,
+    NavShellComponent
   ],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
   exports: [ShellComponent]
 })
 export class LayoutModule {}
