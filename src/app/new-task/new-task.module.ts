a80e7f9d059bb48a0af52d839ed196a9f58c33dc
Alberto Basalo <albertobasalo@hotmail.com>
2019-11-21 16:54:10 +0100

    Add a new task

--- src/app/new-task/new-task.module.ts
+++ src/app/new-task/new-task.module.ts
@@ -3,13 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { NewTaskRoutingModule } from './new-task-routing.module';
 import { NewTaskComponent } from './new-task.component';
+import { FormTaskComponent } from './form-task/form-task.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [NewTaskComponent],
+  declarations: [NewTaskComponent, FormTaskComponent],
   imports: [
     CommonModule,
-    NewTaskRoutingModule
+    NewTaskRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule
   ]
 })
 export class NewTaskModule { }
