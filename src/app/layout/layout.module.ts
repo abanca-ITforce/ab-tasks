04347395b0611863ed7e9e7ac888ed1b4f1d098b
Alberto Basalo <albertobasalo@hotmail.com>
2019-11-21 16:34:01 +0100

    nav-shell

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,17 +1,17 @@
+import { LayoutModule as MaterialLayoutModule } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { NavShellComponent } from './nav-shell/nav-shell.component';
 import { CopyrightComponent } from './shell/copyright/copyright.component';
 import { MainContentComponent } from './shell/main-content/main-content.component';
 import { ShellComponent } from './shell/shell.component';
 import { TopBarComponent } from './shell/top-bar/top-bar.component';
-import { NavShellComponent } from './nav-shell/nav-shell.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -21,7 +21,16 @@ import { MatListModule } from '@angular/material/list';
     MainContentComponent,
     NavShellComponent
   ],
-  imports: [CommonModule, RouterModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
-  exports: [ShellComponent]
+  imports: [
+    CommonModule,
+    RouterModule,
+    MaterialLayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  exports: [NavShellComponent]
 })
 export class LayoutModule {}
