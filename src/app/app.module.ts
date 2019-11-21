cd23900933b147b75936078278bdb120e089e71f
Alberto Basalo <albertobasalo@hotmail.com>
2019-11-21 16:09:22 +0100

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 import { LayoutModule } from './layout/layout.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule, HomeModule],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, HomeModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
