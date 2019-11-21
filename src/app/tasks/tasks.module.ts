c2ca12d90b9bd0981391d51eff5b457f0fdc6177
Alberto Basalo <albertobasalo@hotmail.com>
2019-11-21 17:22:03 +0100

    task card

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,15 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 import { TasksRoutingModule } from './tasks-routing.module';
 import { TasksComponent } from './tasks.component';
 
-
 @NgModule({
   declarations: [TasksComponent],
-  imports: [
-    CommonModule,
-    TasksRoutingModule
-  ]
+  imports: [CommonModule, TasksRoutingModule, MatListModule]
 })
-export class TasksModule { }
+export class TasksModule {}
