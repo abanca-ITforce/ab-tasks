7db0a3e43c43be36a973d6c6f3508802b353790b
Alberto Basalo <albertobasalo@hotmail.com>
2019-11-21 20:40:21 +0100

    button commands

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -10,15 +10,20 @@ export class TaskComponent implements OnInit {
   task = {
     description: '',
     priority: Math.round(Math.random() * 2),
-    dueDate: new Date(),
-    state: Math.round(Math.random() * 2)
+    dueDate: new Date(2019, 11, 5),
+    state: 0
   };
   constructor(activatedRoute: ActivatedRoute) {
     this.task.description = activatedRoute.snapshot.params.id;
   }
   // default, high, tomorrow
   priorityColors = ['primary', 'accent', 'warn'];
-  // pending, completed, inProgress ,discarded
-  stateColors = ['primary', '', 'accent', 'warn'];
+
+  stateCommands = [
+    { id: 0, name: 'Pending', color: 'primary' },
+    { id: 1, name: 'Complete', color: '' },
+    { id: 2, name: 'In Progress', color: 'primary' },
+    { id: 3, name: 'Cancel', color: 'warn' }
+  ];
   ngOnInit() {}
 }
